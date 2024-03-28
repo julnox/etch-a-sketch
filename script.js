d870f77fba7e95bfd67369061daa5a6f16a13abf
Júlio César <52138888+julnox@users.noreply.github.com>
2024-03-28 07:51:46 -0300

    Add color picker event listener

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const divContainer = document.querySelector(".container");
-const button = document.querySelector("button");
+const btnReset = document.querySelector("#btn_reset");
+const colorPicker = document.querySelector("#inp_color");
+const slider = document.querySelector("#inp_size");
+const currentRange = document.querySelector("#curr_size");
+let currColor = colorPicker.value;
 
-function drawGrid(dimension = 16) {
+function drawGrid(dimension) {
   divContainer.replaceChildren();
+  currentRange.textContent = `Size: ${dimension}x${dimension}`;
   let line, item;
 
   for (let i = 0; i < dimension; i++) {
     line = document.createElement("div");
     line.setAttribute("class", "line");
+
     for (let j = 0; j < dimension; j++) {
       item = document.createElement("div");
       item.setAttribute("class", "item");
@@ -15,14 +21,25 @@ function drawGrid(dimension = 16) {
     }
     divContainer.append(line);
   }
+  items = document.querySelectorAll(".line > .item");
+  items.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+      item.style.backgroundColor = currColor;
+    });
+  });
 }
-drawGrid();
 
-button.addEventListener("click", () => {
-  let dimension = Math.floor(+prompt());
-  if (dimension > 1 && dimension <= 100) {
-    drawGrid(dimension);
-  } else {
-    alert("Error: Dimension must be a number between 2 and 100");
-  }
+drawGrid(slider.value);
+
+colorPicker.addEventListener("change", () => {
+  currColor = colorPicker.value;
+});
+
+slider.addEventListener("change", () => {
+  drawGrid(slider.value);
+});
+
+btnReset.addEventListener("click", () => {
+  slider.value = 16;
+  drawGrid(slider.value);
 });
